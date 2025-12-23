47a8ef7fe2d35b90fc59b84f46f2415085ab4bc6
muhammadaffan <muhammadaffanpvt@gmail.com>
2025-12-23 12:49:01 +0530

    Analytics for portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { ToastContainer } from 'react-toastify'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <>
     <ToastContainer />
     <App />
+    <Analytics />
   </>
 
 )
